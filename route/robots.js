3cccfb654521831bda773922dd8ad8503adc5dc7
Ubuntu <ubuntu@ip-172-31-20-223.ec2.internal>
2019-06-17 03:37:37 +0000

    clean up backend

--- route/robots.js
+++ route/robots.js
@@ -1,14 +1,15 @@
 import configPrivate from '../configPrivate.json';
+import config from '../configPublic.json';
 
 exports.index = function(req, res) {
 
     res.header(configPrivate.header.text);
 
-    if (req.app.get('configPrivate').store.getState().isProduction) {
+    if (configPrivate.isProduction) {
         res.send(
 `User-agent: *
 Disallow: /error-404.html
-Sitemap: ` + req.app.get('configPublic').store.getState().www.origin + `/sitemap.xml`
+Sitemap: ` + config.www.origin + `/sitemap.xml`
         );
     } else {
         res.send(
