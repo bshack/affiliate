eab2bf0d0e59c66ccfcf617de5b9f97587ca895e
Ubuntu <ubuntu@ip-172-31-20-223.ec2.internal>
2019-07-18 03:02:40 +0000

    tab support on pdp

--- component/template/productDetail.jsx
+++ component/template/productDetail.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import Picture from './picture.jsx';
+import ProductImages from './productImages.jsx';
 import numeral from 'numeral';
 import UtilityJSONLD from '../../utility/jsonLD';
 import {connect} from 'react-redux';
@@ -12,80 +13,7 @@ class View extends React.PureComponent {
 
         let image;
         if (this.props.data.isAdditionalImageLinkProcessed) {
-            image =
-            <div className="images row no-gutters">
-                <ul role="tablist" className="col-2">
-                    <li key="main" id="tab-1" role="tab" aria-controls="panel-1" aria-selected="true" tabIndex="0">
-                        <div className="image">
-                            <Picture
-                                data={this.props.data}
-                                small={this.props.data.path + '/' + this.props.data.seoFilenamePart
-                                    + '-small'}
-                                medium={this.props.data.path + '/' + this.props.data.seoFilenamePart
-                                    + '-small'}
-                                large={this.props.data.path + '/' + this.props.data.seoFilenamePart
-                                    + '-small'}
-                                xlarge={this.props.data.path + '/' + this.props.data.seoFilenamePart
-                                    + '-small'}
-                                xxlarge={this.props.data.path + '/' + this.props.data.seoFilenamePart
-                                    + '-small'}
-                            />
-                        </div>
-                    </li>
-                    <li key="additional" id="tab-2" role="tab" aria-controls="panel-2"
-                        aria-selected="false" tabIndex="-1">
-                        <div className="image">
-                            <Picture
-                                data={this.props.data}
-                                small={this.props.data.path + '/' + this.props.data.seoFilenamePart
-                                    + '-additional-small'}
-                                medium={this.props.data.path + '/' + this.props.data.seoFilenamePart
-                                    + '-additional-small'}
-                                large={this.props.data.path + '/' + this.props.data.seoFilenamePart
-                                    + '-additional-small'}
-                                xlarge={this.props.data.path + '/' + this.props.data.seoFilenamePart
-                                    + '-additional-small'}
-                                xxlarge={this.props.data.path + '/' + this.props.data.seoFilenamePart
-                                    + '-additional-small'}
-                            />
-                        </div>
-                    </li>
-                </ul>
-                <div  id="panel-1" aria-labelledby="tab-1" role="tabpanel" aria-hidden="false" className="col-10">
-                    <div className="image">
-                        <Picture
-                            data={this.props.data}
-                            small={this.props.data.path + '/' + this.props.data.seoFilenamePart
-                                + '-large'}
-                            medium={this.props.data.path + '/' + this.props.data.seoFilenamePart
-                                + '-large'}
-                            large={this.props.data.path + '/' + this.props.data.seoFilenamePart
-                                + '-large'}
-                            xlarge={this.props.data.path + '/' + this.props.data.seoFilenamePart
-                                + '-large'}
-                            xxlarge={this.props.data.path + '/' + this.props.data.seoFilenamePart
-                                + '-large'}
-                        />
-                    </div>
-                </div>
-                <div id="panel-2" aria-labelledby="tab-2" role="tabpanel" aria-hidden="true" className="col-10">
-                    <div className="image">
-                        <Picture
-                            data={this.props.data}
-                            small={this.props.data.path + '/' + this.props.data.seoFilenamePart
-                                + '-additional-large'}
-                            medium={this.props.data.path + '/' + this.props.data.seoFilenamePart
-                                + '-additional-large'}
-                            large={this.props.data.path + '/' + this.props.data.seoFilenamePart
-                                + '-additional-large'}
-                            xlarge={this.props.data.path + '/' + this.props.data.seoFilenamePart
-                                + '-additional-large'}
-                            xxlarge={this.props.data.path + '/' + this.props.data.seoFilenamePart
-                                + '-additional-large'}
-                        />
-                    </div>
-                </div>
-            </div>;
+            image = <ProductImages data={this.props.data} />;
         } else {
             image =
             <div className="image">
@@ -136,7 +64,7 @@ class View extends React.PureComponent {
 
         let gtin = '';
         if (this.props.data.gtin) {
-            gtin = <div className="gtin">
+            gtin = <div className="col-6 gtin">
                 <strong>gtin</strong>
                 <p>{this.props.data.gtin}</p>
             </div>;
@@ -144,7 +72,7 @@ class View extends React.PureComponent {
 
         let mpn = '';
         if (this.props.data.mpn !== '') {
-            mpn = <div className="mpn">
+            mpn = <div className="col-6 mpn">
                 <strong>mpn</strong>
                 <p>{this.props.data.mpn}</p>
             </div>;
@@ -157,12 +85,15 @@ class View extends React.PureComponent {
 
         let description = '';
         if (this.props.data.description !== '') {
-            description = <p className="description">{this.props.data.description}</p>
+            description = <div className="description">
+                <strong>description</strong>
+                <p>{this.props.data.description}</p>
+            </div>
         }
 
         let brand = '';
         if (this.props.data.brand !== '') {
-            brand = <div className="brand">
+            brand = <div className="col-6 brand">
                 <strong>brand</strong>
                 <p><a href={'/brand/' + this.props.data.brand + '/index.html'}>{this.props.data.brandName}</a></p>
             </div>;
@@ -182,10 +113,16 @@ class View extends React.PureComponent {
                         {price}
                         {productCondition}
                         <a className="anchor-2" href={this.props.data.link}>get deal now</a>
+                        <div className="specifications row">
+                            {brand}
+                            {gtin}
+                            {mpn}
+                            <div className="col-6 vf-id">
+                                <strong>vf id</strong>
+                                <p>{this.props.data.id}</p>
+                            </div>
+                        </div>
                         {description}
-                        {brand}
-                        {gtin}
-                        {mpn}
                     </aside>
                 </div>
                 {utilityJSONLD.product(this.props.data)}
---
+++ component/template/productImages.jsx
@@ -0,0 +1,140 @@
+import React from 'react';
+import Picture from './picture.jsx';
+
+class View extends React.PureComponent {
+
+    constructor(props) {
+        super(props);
+        this.state = {
+            main: true,
+            secondary: false
+        };
+    }
+
+    eventClickTab(e) {
+        e.preventDefault();
+        let target = e.target;
+        if (target.tagName === 'IMG') {
+            target = target.parentNode.parentNode.parentNode;
+        }
+        if (target.id === 'tab-1') {
+            this.setState({
+                main: true,
+                secondary: false
+            });
+        } else {
+            this.setState({
+                main: false,
+                secondary: true
+            });
+        }
+    }
+
+    render() {
+        return (
+            <div className="images row no-gutters">
+                <ul role="tablist" className="col-2">
+                    <li
+                        key="main"
+                        id="tab-1"
+                        role="tab"
+                        aria-controls="panel-1"
+                        aria-selected={(this.state.main? 'true' : 'false')}
+                        tabIndex={(this.state.main? '0' : '-1')}
+                        onClick={this.eventClickTab.bind(this)}
+                    >
+                        <div className="image">
+                            <Picture
+                                data={this.props.data}
+                                small={this.props.data.path + '/' + this.props.data.seoFilenamePart
+                                    + '-small'}
+                                medium={this.props.data.path + '/' + this.props.data.seoFilenamePart
+                                    + '-small'}
+                                large={this.props.data.path + '/' + this.props.data.seoFilenamePart
+                                    + '-small'}
+                                xlarge={this.props.data.path + '/' + this.props.data.seoFilenamePart
+                                    + '-small'}
+                                xxlarge={this.props.data.path + '/' + this.props.data.seoFilenamePart
+                                    + '-small'}
+                            />
+                        </div>
+                    </li>
+                    <li
+                        key="additional"
+                        id="tab-2"
+                        role="tab"
+                        aria-controls="panel-2"
+                        aria-selected={(this.state.secondary? 'true' : 'false')}
+                        tabIndex={(this.state.secondary? '0' : '-1')}
+                        onClick={this.eventClickTab.bind(this)}
+                    >
+                        <div className="image">
+                            <Picture
+                                data={this.props.data}
+                                small={this.props.data.path + '/' + this.props.data.seoFilenamePart
+                                    + '-additional-small'}
+                                medium={this.props.data.path + '/' + this.props.data.seoFilenamePart
+                                    + '-additional-small'}
+                                large={this.props.data.path + '/' + this.props.data.seoFilenamePart
+                                    + '-additional-small'}
+                                xlarge={this.props.data.path + '/' + this.props.data.seoFilenamePart
+                                    + '-additional-small'}
+                                xxlarge={this.props.data.path + '/' + this.props.data.seoFilenamePart
+                                    + '-additional-small'}
+                            />
+                        </div>
+                    </li>
+                </ul>
+                <div
+                    id="panel-1"
+                    aria-labelledby="tab-1"
+                    role="tabpanel"
+                    aria-hidden={(this.state.main? 'false' : 'true')}
+                    className="col-10"
+                >
+                    <div className="image">
+                        <Picture
+                            data={this.props.data}
+                            small={this.props.data.path + '/' + this.props.data.seoFilenamePart
+                                + '-large'}
+                            medium={this.props.data.path + '/' + this.props.data.seoFilenamePart
+                                + '-large'}
+                            large={this.props.data.path + '/' + this.props.data.seoFilenamePart
+                                + '-large'}
+                            xlarge={this.props.data.path + '/' + this.props.data.seoFilenamePart
+                                + '-large'}
+                            xxlarge={this.props.data.path + '/' + this.props.data.seoFilenamePart
+                                + '-large'}
+                        />
+                    </div>
+                </div>
+                <div
+                    id="panel-2"
+                    aria-labelledby="tab-2"
+                    role="tabpanel"
+                    aria-hidden={(this.state.secondary? 'false' : 'true')}
+                    className="col-10"
+                >
+                    <div className="image">
+                        <Picture
+                            data={this.props.data}
+                            small={this.props.data.path + '/' + this.props.data.seoFilenamePart
+                                + '-additional-large'}
+                            medium={this.props.data.path + '/' + this.props.data.seoFilenamePart
+                                + '-additional-large'}
+                            large={this.props.data.path + '/' + this.props.data.seoFilenamePart
+                                + '-additional-large'}
+                            xlarge={this.props.data.path + '/' + this.props.data.seoFilenamePart
+                                + '-additional-large'}
+                            xxlarge={this.props.data.path + '/' + this.props.data.seoFilenamePart
+                                + '-additional-large'}
+                        />
+                    </div>
+                </div>
+            </div>
+        );
+    }
+
+}
+
+export default View;
